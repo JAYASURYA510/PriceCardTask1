c72c4f26925ddf81ff1813cd46789d495fdcb18c
JAYASURYA510 <suryatheforgiver@gmail.com>
2023-07-03 22:35:55 +0530

    PriceCardTask1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
 import './App.css';
+import PriceCard from './components/Pricecard/pricecard';
 
+const pricingDetails = [
+  {
+    pricing: 0,
+    priceType: "FREE",
+    features: [
+      { feature: "Single User", isAvailable: true },
+      { feature: "5GB Storage", isAvailable: true },
+      { feature: "Unlimited Public Projects", isAvailable: true },
+      { feature: "Community Access", isAvailable: true },
+      { feature: "Unlimited Private Projects", isAvailable: false },
+      { feature: "Dedicated Phone Support", isAvailable: false },
+      { feature: "Free Subdomain", isAvailable: false },
+      { feature: "Monthly Status Reports", isAvailable: false },
+    ],
+  },
+  {
+    pricing: 100,
+    priceType: "PAID",
+    features: [
+      { feature: "Single User", isAvailable: true },
+      { feature: "5GB Storage", isAvailable: true },
+      { feature: "Unlimited Public Projects", isAvailable: true },
+      { feature: "Community Access", isAvailable: true },
+      { feature: "Unlimited Private Projects", isAvailable: true },
+      { feature: "Dedicated Phone Support", isAvailable: true },
+      { feature: "Free Subdomain", isAvailable: false },
+      { feature: "Monthly Status Reports", isAvailable: false },
+    ],
+  },
+  {
+    pricing: 300,
+    priceType: "PAID",
+    features: [
+      { feature: "Single User", isAvailable: true },
+      { feature: "5GB Storage", isAvailable: true },
+      { feature: "Unlimited Public Projects", isAvailable: true },
+      { feature: "Community Access", isAvailable: true },
+      { feature: "Unlimited Private Projects", isAvailable: true },
+      { feature: "Dedicated Phone Support", isAvailable: true },
+      { feature: "Free Subdomain", isAvailable: true },
+      { feature: "Monthly Status Reports", isAvailable: true },
+    ],
+  },
+];
+
+// FUNCTIONAL COMPONENT
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <section className="pricing py-5">
+        <div className="container">
+          <div className="row">
+            {pricingDetails.map((d, i) => (
+              <PriceCard data={d} key={`pricing-card-${i}`} />
+            ))}
+          </div>
+        </div>
+      </section>
     </div>
   );
 }
+
 
 export default App;
